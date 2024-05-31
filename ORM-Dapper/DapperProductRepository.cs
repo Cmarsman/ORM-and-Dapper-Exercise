791737ea035d0152e4bc8a9b810ec76a4c1715d4
Cmarsman <chris_marsden@yahoo.com>
2024-05-31 14:55:48 -0500

    added exercise 2

---
+++ ORM-Dapper/DapperProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public  DapperProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products;");
+        }
+
+
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _conn.Execute("INSERT INTO products (Name, Price, CategoryID) VALUES (@eachParameter);");
+        }
+
+        
+        
+
+    }
+}
